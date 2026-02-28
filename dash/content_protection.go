725659048822f5d562d640ede1f99d46cf7e9985
27 <27@riseup.net>
2026-02-27 19:12:53 -0600

    +   "version": "v1.33.6"

--- dash/content_protection.go
+++ dash/content_protection.go
@@ -1,25 +1,12 @@
 package dash
 
-import (
-   "encoding/base64"
-   "encoding/hex"
-   "strings"
-)
+import "encoding/base64"
 
 // ContentProtection specifies DRM schemes.
 type ContentProtection struct {
    SchemeIdUri string `xml:"schemeIdUri,attr"`
    DefaultKid  string `xml:"default_KID,attr"`
    Pssh        string `xml:"pssh"`
-}
-
-// GetDefaultKid returns the DefaultKid as a byte slice.
-func (cp *ContentProtection) GetDefaultKid() ([]byte, error) {
-   if cp.DefaultKid == "" {
-      return nil, nil
-   }
-   clean := strings.ReplaceAll(cp.DefaultKid, "-", "")
-   return hex.DecodeString(clean)
 }
 
 // GetPssh returns the PSSH data as a byte slice.
