2a6c318b57357c3eefcb565a68c252ceb03d88e5
27 <27@riseup.net>
2026-03-18 17:09:40 -0500

    +func ParseRange(input string) (uint64, uint64, error) {

--- dash/segment_base.go
+++ dash/segment_base.go
@@ -7,6 +7,30 @@ import (
    "strings"
 )
 
+func ParseRange(input string) (uint64, uint64, error) {
+   startStr, endStr, found := strings.Cut(input, "-")
+   if !found {
+      return 0, 0, errors.New("invalid range format")
+   }
+   start, err := strconv.ParseUint(startStr, 10, 64)
+   if err != nil {
+      return 0, 0, err
+   }
+   end, err := strconv.ParseUint(endStr, 10, 64)
+   if err != nil {
+      return 0, 0, err
+   }
+   return start, end, nil
+}
+
+func FormatRange(start, end uint64) string {
+   var sb strings.Builder
+   sb.WriteString(strconv.FormatUint(start, 10))
+   sb.WriteByte('-')
+   sb.WriteString(strconv.FormatUint(end, 10))
+   return sb.String()
+}
+
 // Initialization contains URL and byte range information for initialization segments.
 type Initialization struct {
    // Used in SegmentBase
@@ -33,28 +57,4 @@ func (i *Initialization) ResolveSourceUrl() (*url.URL, error) {
 type SegmentBase struct {
    IndexRange     string          `xml:"indexRange,attr"`
    Initialization *Initialization `xml:"Initialization"`
-}
-
-func ParseRange(input string) (uint64, uint64, error) {
-   startStr, endStr, found := strings.Cut(input, "-")
-   if !found {
-      return 0, 0, errors.New("invalid range format")
-   }
-   start, err := strconv.ParseUint(startStr, 10, 64)
-   if err != nil {
-      return 0, 0, err
-   }
-   end, err := strconv.ParseUint(endStr, 10, 64)
-   if err != nil {
-      return 0, 0, err
-   }
-   return start, end, nil
-}
-
-func FormatRange(start, end uint64) string {
-   var sb strings.Builder
-   sb.WriteString(strconv.FormatUint(start, 10))
-   sb.WriteByte('-')
-   sb.WriteString(strconv.FormatUint(end, 10))
-   return sb.String()
 }
