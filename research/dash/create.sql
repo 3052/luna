4548d8a664547552d6912074000ff8641274fc0d
27 <27@riseup.net>
2026-09-20 13:14:25 -0500

    +   id           TEXT,

--- research/dash/create.sql
+++ research/dash/create.sql
@@ -1,10 +1,11 @@
 -- create.sql
+
 CREATE TABLE mpd (
    base_url TEXT
 );
 
 CREATE TABLE period (
-   id           TEXT PRIMARY KEY,
+   id           TEXT,
    duration     TEXT
 );
 
@@ -12,32 +13,29 @@ CREATE TABLE adaptation_set (
    id        TEXT NOT NULL,
    period_id TEXT NOT NULL,
    lang      TEXT,
-   label     TEXT,
-   PRIMARY KEY (id, period_id)
+   label     TEXT
 );
 
 CREATE TABLE representation (
-   id                  TEXT NOT NULL,
-   period_id           TEXT NOT NULL,
-   adaptation_set_id   TEXT NOT NULL,
-   codecs              TEXT,
-   bandwidth           INTEGER NOT NULL,
-   mime_type           TEXT,
-   width               INTEGER,
-   height              INTEGER,
-   base_url            TEXT,
-   PRIMARY KEY (id, period_id)
+   id                TEXT NOT NULL,
+   period_id         TEXT NOT NULL,
+   adaptation_set_id TEXT NOT NULL,
+   codecs            TEXT,
+   bandwidth         INTEGER NOT NULL,
+   mime_type         TEXT,
+   width             INTEGER,
+   height            INTEGER,
+   base_url          TEXT
 );
 
 CREATE TABLE segment_template (
-   adaptation_set_id TEXT NOT NULL,
-   period_id         TEXT NOT NULL,
-   duration          INTEGER,
-   media             TEXT,
+   adaptation_set_id        TEXT NOT NULL,
+   period_id                TEXT NOT NULL,
+   duration                 INTEGER,
+   media                    TEXT,
    presentation_time_offset INTEGER,
-   start_number      INTEGER,
-   timescale         INTEGER,
-   PRIMARY KEY (adaptation_set_id, period_id)
+   start_number             INTEGER,
+   timescale                INTEGER
 );
 
 CREATE TABLE segment_timeline (
@@ -45,19 +43,18 @@ CREATE TABLE segment_timeline (
    period_id         TEXT NOT NULL,
    position          INTEGER NOT NULL,
    d                 INTEGER NOT NULL,
-   r                 INTEGER NOT NULL DEFAULT 0,
-   PRIMARY KEY (adaptation_set_id, period_id, position)
+   r                 INTEGER NOT NULL DEFAULT 0
 );
 
 CREATE TABLE content_protection (
    adaptation_set_id TEXT NOT NULL,
    scheme_id_uri     TEXT NOT NULL,
-   pssh              TEXT,
-   PRIMARY KEY (adaptation_set_id, scheme_id_uri)
+   pssh              TEXT
 );
 
 CREATE TABLE role (
-   adaptation_set_id TEXT PRIMARY KEY,
+   adaptation_set_id TEXT,
    value             TEXT
 );
+
 -- create.sql
